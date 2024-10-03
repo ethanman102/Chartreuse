3ee4ab83d685c80bd0599cb337cffa892ad138be
Ethan Keys <ekeys@cs.ualberta.ca>
2024-10-03 10:59:38 -0600

    fixed errors

--- chartreuse/views.py
+++ chartreuse/views.py
@@ -1,4 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.views import generic
+from django.contrib.auth.models import User as AuthUser
+from django.urls import reverse
+from .models import User
+from django.views.generic.detail import DetailView
 
 class Host():
     host = "https://f24-project-chartreuse-b4b2bcc83d87.herokuapp.com/api/"

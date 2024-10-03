09f7be188eff0cdd0bb94909815b6ae2ee2710b2
Julia-Dantas <jdantas@ualberta.ca>
2024-10-03 09:30:30 -0600

    Added missing api/ to path

--- chartreuse/urls.py
+++ chartreuse/urls.py
@@ -8,14 +8,14 @@ urlpatterns = [
     path("api/author/", users.create_user, name="create_user"),
     path("api/author/login/", users.login_user, name="login_user"),
 
-    path("authors/<str:user_id>/inbox/", likes.like, name="like"),
-    path("authors/<str:user_id>/posts/<str:post_id>/likes", likes.likes, name="likes"),
-    path("authors/<str:user_id>/posts/<str:post_id>/comments/<str:comment_id>/likes", likes.comment_likes, name="comment_likes"),
+    path("api/authors/<str:user_id>/inbox/", likes.like, name="like"),
+    path("api/authors/<str:user_id>/posts/<str:post_id>/likes", likes.likes, name="likes"),
+    path("api/authors/<str:user_id>/posts/<str:post_id>/comments/<str:comment_id>/likes", likes.comment_likes, name="comment_likes"),
 
-    path("authors/<str:user_id>/liked/", likes.liked, name="get_liked"),
-    path("authors/<str:user_id>/liked/<str:like_id>", likes.like_object, name="get_like_object"),
+    path("api/authors/<str:user_id>/liked/", likes.liked, name="get_liked"),
+    path("api/authors/<str:user_id>/liked/<str:like_id>", likes.like_object, name="get_like_object"),
 
-    path('authors/<int:author_id>/posts/<int:post_id>/image', images.get_image_post, name='get_image_post'),
+    path('api/authors/<int:author_id>/posts/<int:post_id>/image', images.get_image_post, name='get_image_post'),
 
     path("github/<str:user_id>/events/", github.get_events, name="get_events"),
     path("github/<str:user_id>/starred/", github.get_starred, name="get_starred"),

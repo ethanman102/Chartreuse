81608801ace8538c4a142515d1b6fc345c36f8ea
ethanman102 <ethankey.s@yahoo.com>
2024-10-12 14:57:38 -0600

    fixed url errors and tests

--- chartreuse/urls.py
+++ chartreuse/urls.py
@@ -8,9 +8,10 @@ app_name = "chartreuse"
 urlpatterns = [
     path("api/author/login/", users.UserViewSet.login_user, name="login_user"),
 
-    path("authors/<str:pk>/", ProfileDetailView.as_view(),name="profile"),
-    path("authors/<str:followee>/accept/<str:follower>", views.follow_accept,name="profile_follow_accept"),
-    path("authors/<str:followee>/reject/<str:follower>", views.follow_reject,name="profile_follow_reject"),
+    re_path(r"authors/accept/(?P<followed>.+)/(?P<follower>.+)/", views.follow_accept,name="profile_follow_accept"),
+    re_path(r"authors/reject/(?P<followed>.+)/(?P<follower>.+)/", views.follow_reject,name="profile_follow_reject"),
+    re_path(r"authors/(?P<url_id>.+)/", ProfileDetailView.as_view(),name="profile"),
+    
 
     re_path(r"api/authors/(?P<user_id>.+)/inbox/", likes.LikeViewSet.as_view({'post': 'add_like', 'delete': 'remove_like'}), name="like"),
     re_path(r"api/authors/(?P<user_id>.+)/posts/(?P<post_id>.+)/likes", likes.LikeViewSet.get_post_likes, name="post_likes"),

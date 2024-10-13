f7562761a565f8262b9ed92ac5cbb083ae281a52
Julia-Dantas <jdantas@ualberta.ca>
2024-10-13 00:27:00 -0600

    css for landing page

--- chartreuse_admin/settings.py
+++ chartreuse_admin/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,6 +22,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-f8ao_^5wudmpvgt3iy(kr^5nojkqm+hjyw*7x-av!ag&gqqxw_'
+
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), "static")
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'), ]
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True

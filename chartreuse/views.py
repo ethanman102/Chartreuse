2b1dae537f57d529a39902ab8fc7694d3b0520f1
Theresa Tian <theresagt02@gmail.com>
2024-10-27 19:38:21 -0600

    commonmark rendering the whole string now (need to keep the commonmark references page open)

--- chartreuse/views.py
+++ chartreuse/views.py
@@ -32,7 +32,7 @@ def test(request):
     Arguments:
     request: Request object
     '''
-    test_str = " and **goodbye**"
+    test_str = "# Hello and **goodbye** "
     return render(request, 'test.html', {'markdown_txt': test_str})
 
 

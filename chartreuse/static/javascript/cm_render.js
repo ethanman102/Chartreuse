10de57c7ebe9d98dfc6fefa40417fbc40aef7fbf
theresa tian <theresagt02@gmail.com>
2024-11-01 14:58:16 -0600

    removed commented code from cm_render.js

--- chartreuse/static/javascript/cm_render.js
+++ chartreuse/static/javascript/cm_render.js
@@ -1,7 +1,5 @@
 'use strict'
 
-// ISSUE: somewhere, the contentType is being saved as "commonmark" instead of "text/commonmark"
-
 // waiting for the page to load
 document.addEventListener("DOMContentLoaded", function() {
     const commonmark = window.commonmark;
@@ -28,36 +26,3 @@ document.addEventListener("DOMContentLoaded", function() {
     // // sets the text to be shown in html file as the rendered Commonmark text 
     document.getElementById("cm_text").innerHTML = result;
 })
-
-// function main() {
-//     const commonmark = window.commonmark;
-
-//     // variable to track the html element for displaying the commonmark text
-//     // NOTE - debugger says cm_text is empty.
-//     // POSSIBILITY: text/commonmark is never properly assigned 
-//     var markString = document.getElementById('cm_text').innerText;
-
-//     // for debugging 
-//     console.log(markString);
-
-
-//     // will need to bundle with esbuild command for the browser import mini package
-//     var reader = new commonmark.Parser({smart: true});
-//     var writer = new commonmark.HtmlRenderer({sourcepos: true, safe: true, softbreak: "<br />"});
-//     // var writer = new commonmark.HtmlRenderer({sourcepos: true, safe: true, softbreak: "<br />"});
-//     var parsed = reader.parse(markString); // parsed is a 'Node' tree
-//     // transform parsed if you like...
-//     var result = writer.render(parsed); // result is a String
-
-//     // for debugging 
-//     console.log(result);
-    
-//     // // sets the text to be shown in html file as the rendered Commonmark text 
-//     document.getElementById('cm_text').innerHTML = result;
-// };
-
-// in case I need to bundle the JS code later
-// npx esbuild /chartreuse/static/javascript/cm_render.js --bundle --minify --sourcemap --outfile=./chartreuse/static/cm_render.min.js
-
-// add this to citations page later 
-// https://stackoverflow.com/questions/42237388/syntaxerror-import-declarations-may-only-appear-at-top-level-of-a-module

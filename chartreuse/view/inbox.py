74324bd3fc2540c9421a1e086f63fdc223dcfd76
Julia-Dantas <jdantas@ualberta.ca>
2024-11-14 23:29:08 -0700

    Update inbox.py

--- chartreuse/view/inbox.py
+++ chartreuse/view/inbox.py
@@ -125,7 +125,7 @@ def inbox(request, user_id):
         print(comment_text)
         print(contentType)
         print(new_post)
-        comment = Comment.objects.filter(user=comment_author, comment=comment_text, contentType=contentType, post=new_post).first()
+        comment = Comment.objects.filter(url_id=comment_id).first()
 
         if comment is None:
             comment = Comment.objects.create(user=comment_author, comment=comment_text, url_id=comment_id, contentType=contentType, post=new_post)

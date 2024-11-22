543eb0ef5bd34a43dcb8939154a30944370969a7
Julia-Dantas <jdantas@ualberta.ca>
2024-11-21 21:54:23 -0700

    Update discover_view.py

--- chartreuse/view/discover_view.py
+++ chartreuse/view/discover_view.py
@@ -88,9 +88,9 @@ class DiscoverAuthorListView(ListView):
             'size':PAGE_SIZE
         }   
 
-        print("URL", url)
+        print("URL",url)
 
-        response = requests.get(url, params=params)
+        response = requests.get(url, params=params, auth=(username,password))
         print(response.status_code)
         print(response.content)
         

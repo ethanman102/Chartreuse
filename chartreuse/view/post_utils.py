354f3474ad446333b8ee8008841f4c78295aad83
ethanman102 <107374579+ethanman102@users.noreply.github.com>
2024-11-23 14:13:05 -0700

    fixing images ahh

--- chartreuse/view/post_utils.py
+++ chartreuse/view/post_utils.py
@@ -633,6 +633,8 @@ def get_image_post(pfp_url):
         if pfp_post and pfp_post.content and pfp_post.contentType in ['image/jpeg;base64', 'image/png;bas64', 'image/webp', 'image/jpg;base64']:
             if not pfp_post.content.startswith('data:'):
                 pfp_url = f"data:{pfp_post.contentType};charset=utf-8;base64, {pfp_post.content}"
+            else:
+                pfp_url = pfp_post.content
         else:
             pfp_url = f"{Host.host}/static/images/default_pfp_1.png"
         return pfp_url

92df214c3b8f16fd3a0c168b78fdc469f3085419
ethanman102 <107374579+ethanman102@users.noreply.github.com>
2024-11-23 14:27:35 -0700

    match pattern group

--- chartreuse/view/post_utils.py
+++ chartreuse/view/post_utils.py
@@ -620,7 +620,7 @@ def check_duplicate_post(request):
     
 def get_image_post(pfp_url):
     print(pfp_url,'heyyy')
-    pattern = r"(?P<host>https?:\/\/.+?herokuapp\.com)\/authors\/(?P<author_serial>\d+)\/posts\/(?P<post_serial>\d+)\/image"
+    pattern = r"(?P<host>https?:\/\/.+?herokuapp\.com)(\/chartreuse\/api)?\/authors\/(?P<author_serial>\d+)\/posts\/(?P<post_serial>\d+)\/image"
     match = re.search(pattern, pfp_url)
 
     if match:

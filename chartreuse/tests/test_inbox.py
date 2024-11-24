dcca83d05b6b2879ab4ece3026a1ba3fdeed6d57
ethanman102 <ethankey.s@yahoo.com>
2024-11-23 23:52:37 -0700

    inbox tests

--- chartreuse/tests/test_inbox.py
+++ chartreuse/tests/test_inbox.py
@@ -2,7 +2,7 @@ from django.test import TestCase
 from django.urls import reverse
 from rest_framework.test import APIClient
 from chartreuse.views import Host
-from ..models import User, Node,Post,Comment
+from ..models import User, Node,Post,Comment,Like
 import base64
 import json
 from django.contrib.auth.models import User as AuthUser
@@ -317,16 +317,226 @@ class AuthenticationTestCases(TestCase):
         self.assertEqual(comment.user,author_queryset_comment[0])
 
 
-    '''
+    
     def test_new_post_with_likes(self):
-        pass
+        postObject = {
+                "type": "post",
+                "title": 'ETHAN TITLE',
+                "id": 'https://FQID/NEW',
+                "description": 'ETHAN DESCRIPTION',
+                "contentType": 'text/plain',
+                "content": 'This is ethans test post',
+                "author": {
+                    "type": "author",
+                    "id": 'https://newest_author/2/2/2',
+                    "page": 'fillerdata',
+                    "host": 'https://newest_author/',
+                    "displayName": 'ETHANAUTHOR',
+                    "github": '',
+                    "profileImage": 'fakeimg.png'
+                },
+                "comments":{
+                    "type": "comments",
+                    "src": []
+                },
+                "likes": {
+                    "types": "likes",
+                    "src": [
+                        {
+                            "type": "like",
+                            "author": {
+                                "type": "author",
+                                "id": 'https://newest_author/2/a/3',
+                                "page": 'whateverpage',
+                                "host": 'https://woah',
+                                "displayName": 'ETHANLIKE',
+                                "github": '',
+                                "profileImage": 'testimg.png',
+                            },
+                            "published": datetime.now(),
+                            "id": 'https://likeurlid/',
+                            "object": 'https://FQID/NEW'
+                        }
+                    ]
+                },
+                "published": datetime.now(),
+                "visibility": 'PUBLIC',
+            }
+        
+        
+        response = self.client.post(reverse('chartreuse:inbox',args=[quote('http://f24-project-chartreuse-b4b2bcc83d87.herokuapp.com/chartreuse/api/authors/1',safe='')]), postObject, content_type='application/json',headers=self.creds)
+        # check that items got added.
+        self.assertEqual(response.status_code,200)
+        
+        data = json.loads(response.content)
+
+        self.assertNotEqual(data.get('status'),None)
+        self.assertEqual(data.get('status'),"Post added successfully")
+
+        # check that the new author was added.
+
+        author_queryset = User.objects.filter(url_id='https://newest_author/2/2/2')
+        self.assertTrue(author_queryset.exists())
+        self.assertEqual(author_queryset.count(),1)
+
+        # check that the post was added.
+
+        post_queryset = Post.objects.filter(url_id='https://FQID/NEW')
+        self.assertTrue(post_queryset.exists())
+        self.assertEqual(post_queryset.count(),1)
+
+        new_post = post_queryset[0]
+
+        self.assertEqual(new_post.title,'ETHAN TITLE')
+        self.assertEqual(new_post.content,'This is ethans test post')
+        self.assertEqual(new_post.contentType,'text/plain')
+        self.assertEqual(new_post.description,'ETHAN DESCRIPTION')
+        self.assertEqual(new_post.user,author_queryset[0])
+
+        # check like and like author got added
+        author_queryset_like = User.objects.filter(url_id='https://newest_author/2/a/3')
+        self.assertTrue(author_queryset_like.exists())
+        self.assertEqual(author_queryset_like.count(),1)
+
+        # check that the like got added
+
+        like_queryset = Like.objects.filter(url_id='https://likeurlid/')
+        self.assertTrue(like_queryset.exists())
+        self.assertEqual(like_queryset.count(),1)
+
+        like = like_queryset[0]
+        self.assertEqual(like.user,author_queryset_like[0])
 
     def test_new_post_like(self):
-        pass
+        postObject = {
+                "type": "post",
+                "title": 'ETHAN TITLE',
+                "id": 'https://FQID/NEW',
+                "description": 'ETHAN DESCRIPTION',
+                "contentType": 'text/plain',
+                "content": 'This is ethans test post',
+                "author": {
+                    "type": "author",
+                    "id": 'https://newest_author/2/2/2',
+                    "page": 'fillerdata',
+                    "host": 'https://newest_author/',
+                    "displayName": 'ETHANAUTHOR',
+                    "github": '',
+                    "profileImage": 'fakeimg.png'
+                },
+                "comments":{
+                    "type": "comments",
+                    "src": []
+                },
+                "likes": {
+                    "types": "likes",
+                    "src": [
+                    ]
+                },
+                "published": datetime.now(),
+                "visibility": 'PUBLIC',
+            }
+        
+        
+        self.client.post(reverse('chartreuse:inbox',args=[quote('http://f24-project-chartreuse-b4b2bcc83d87.herokuapp.com/chartreuse/api/authors/1',safe='')]), postObject, content_type='application/json',headers=self.creds)
 
-    def test_new_comment_like(self):
-        pass
+        like_object = {
+            "type": "like",
+            "author": {
+                "type": "author",
+                "id": 'https://newest_author/2/a/3',
+                "page": 'whateverpage',
+                "host": 'https://woah',
+                "displayName": 'ETHANLIKE',
+                "github": '',
+                "profileImage": 'testimg.png',
+            },
+            "published": datetime.now(),
+            "id": 'https://likeurlid/',
+            "object": 'https://FQID/NEW'
+        }
+        self.client.post(reverse('chartreuse:inbox',args=[quote('http://f24-project-chartreuse-b4b2bcc83d87.herokuapp.com/chartreuse/api/authors/1',safe='')]), like_object, content_type='application/json',headers=self.creds)
+
+        # check that the like author got added
+        author_queryset_like = User.objects.filter(url_id='https://newest_author/2/a/3')
+        self.assertTrue(author_queryset_like.exists())
+        self.assertEqual(author_queryset_like.count(),1)
+
+        # check that the like got added
+
+        like_queryset = Like.objects.filter(url_id='https://likeurlid/')
+        self.assertTrue(like_queryset.exists())
+        self.assertEqual(like_queryset.count(),1)
+
+        like = like_queryset[0]
+        self.assertEqual(like.user,author_queryset_like[0])
+
 
     def test_new_comment(self):
-        pass
-    '''
+        postObject = {
+                "type": "post",
+                "title": 'ETHAN TITLE',
+                "id": 'https://FQID/NEW',
+                "description": 'ETHAN DESCRIPTION',
+                "contentType": 'text/plain',
+                "content": 'This is ethans test post',
+                "author": {
+                    "type": "author",
+                    "id": 'https://newest_author/2/2/2',
+                    "page": 'fillerdata',
+                    "host": 'https://newest_author/',
+                    "displayName": 'ETHANAUTHOR',
+                    "github": '',
+                    "profileImage": 'fakeimg.png'
+                },
+                "comments":{
+                    "type": "comments",
+                    "src": []
+                },
+                "likes": {
+                    "types": "likes",
+                    "src": []
+                },
+                "published": datetime.now(),
+                "visibility": 'PUBLIC',
+            }
+        
+        
+        self.client.post(reverse('chartreuse:inbox',args=[quote('http://f24-project-chartreuse-b4b2bcc83d87.herokuapp.com/chartreuse/api/authors/1',safe='')]), postObject, content_type='application/json',headers=self.creds)
+
+        comment_obj = {
+                        "type": "comment",
+                        "author": {
+                            "type": "author",
+                            "id": 'https://newest_author/2/a/3',
+                            "page": 'whateverpage',
+                            "host": 'https://woah',
+                            "displayName": 'comment author',
+                            "github": '',
+                            "profileImage": 'testtest.png',
+                        },
+                        "comment": 'NEW COMMENT HI',
+                        "contentType": 'text/plain',
+                        "published": datetime.now(),
+                        "id": 'http://newcomment/',
+                        "post": 'https://FQID/NEW',
+                    }
+        
+        self.client.post(reverse('chartreuse:inbox',args=[quote('http://f24-project-chartreuse-b4b2bcc83d87.herokuapp.com/chartreuse/api/authors/1',safe='')]), comment_obj, content_type='application/json',headers=self.creds)
+        
+        author_queryset_comment = User.objects.filter(url_id='https://newest_author/2/a/3')
+        self.assertTrue(author_queryset_comment.exists())
+        self.assertEqual(author_queryset_comment.count(),1)
+        
+        comment_queryset = Comment.objects.filter(url_id='http://newcomment/')
+
+        self.assertTrue(comment_queryset.exists())
+        self.assertEqual(comment_queryset.count(),1)
+        comment = comment_queryset[0]
+
+        self.assertEqual(comment.comment,'NEW COMMENT HI')
+        self.assertEqual(comment.contentType,'text/plain')
+        self.assertEqual(comment.user,author_queryset_comment[0])
+
+
+  

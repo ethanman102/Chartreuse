c9704924b058b20481c9b993583b51e380c7766b
ethanman102 <ethankey.s@yahoo.com>
2024-11-23 21:53:09 -0700

    Update test_authentication.py

--- chartreuse/tests/test_authentication.py
+++ chartreuse/tests/test_authentication.py
@@ -1,6 +1,5 @@
 from django.test import TestCase
 from django.urls import reverse
-from urllib.parse import quote
 from rest_framework.test import APIClient
 from chartreuse.views import Host
 from ..models import User, Node
@@ -21,36 +20,9 @@ class AuthenticationTestCases(TestCase):
 
         cls.client = APIClient()
 
-        # Test user data
-        cls.test_user_1_data = {
-            'displayName': 'Greg Johnson',
-            'github': 'http://github.com/gjohnson',
-            'profileImage': 'https://i.imgur.com/k7XVwpB.jpeg',
-            'username': 'greg',
-            'password': 'ABC123!!!',
-            'host': cls.host,
-            'firstName': 'Greg',
-            'lastName': 'Johnson',
-        }
-
-        cls.test_user_2_data = {
-            'displayName': 'John Smith',
-            'github': 'http://github.com/jiori',
-            'profileImage': 'https://i.imgur.com/1234.jpeg',
-            'username': 'john',
-            'password': '87@398dh817b!',
-            'host': cls.host,
-            'firstName': 'John',
-            'lastName': 'Smith',
-        }
-
         # create test node with the correct auth!!
         cls.node = Node.objects.create(host='http://f24-project-chartreuse-b4b2bcc83d87.herokuapp.com/',username='abc',password='123',follow_status='INCOMING',status='ENABLED')
         cls.creds = {'Authorization' : 'Basic ' + base64.b64encode(b'abc:123').decode('utf-8')}
-
-        # Create test users
-        #cls.client.post(reverse('chartreuse:user-list'), cls.test_user_1_data, format='json', headers=cls.creds)
-        #cls.client.post(reverse('chartreuse:user-list'), cls.test_user_2_data, format='json', headers=cls.creds)
 
 
 
@@ -61,6 +33,9 @@ class AuthenticationTestCases(TestCase):
     
     def test_successful_incoming_authentication(self):
         # https://stackoverflow.com/questions/18869074/create-url-without-request-execution
+        # Stack overflow post: Create url without request execution
+        # Purpose: How to create a python requests object without actually making a request to a url.
+        # Answered by: Lukasa on September 18, 2013 (Use requests.Request then .prepare())
         request = Request(url='https://None',auth=('abc','123')).prepare()
         response = checkIfRequestAuthenticated(request)
        
